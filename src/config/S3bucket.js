12bb57ace1343bc95409df23fe55155e3ec03bfb
adyeetya <2612adityasingh2000@gmail.com>
2025-07-16 11:22:06 +0530

    new changes

--- src/config/S3bucket.js
+++
@@ -1,7 +0,0 @@
-export const config = {
-    bucketName: "mobileapplications",
-    dirName: "photo",
-    region: "ap-south-1",
-    accessKeyId: "AKIAQKGGXOAWXVFSQCGZ",
-    secretAccessKey: "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"
-  };

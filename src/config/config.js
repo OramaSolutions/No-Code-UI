6cd85a4a64cbf50a0d54a548427fd75e520fe60b
adyeetya <2612adityasingh2000@gmail.com>
2025-07-19 14:11:56 +0530

    to check deployment

--- src/config/config.js
+++ src/config/config.js
@@ -2,12 +2,12 @@ module.exports = {
     Url: 'https://nocode-node.oramasolutions.in/api/v1/',
     // Url: 'http://192.168.2.99:3100/api/v1/',
     // Url2: 'https://nocode-defect-detection.oramasolutions.in/',
-    Url2: 'http://192.168.1.187:5000/',
+   
 
     getUrl: function (key) {
         const urls = {
             'defect-detection': 'https://nocode-defect-detection.oramasolutions.in/',
-            'classification': 'http://192.168.1.187:5000/',
+            'classification': 'https://nocode-classification.oramasolutions.in/',
             'text-extraction': 'https://nocode-text-extraction.oramasolutions.in/',
             'object-detection': 'https://nocode-object-detection.oramasolutions.in/'
         };

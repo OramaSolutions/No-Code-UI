2a373cf7f338dd41960c6f1aeaac25a971e11cc7
adyeetya <2612adityasingh2000@gmail.com>
2025-10-08 10:55:21 +0530

    application page hcheck for token

--- src/componet/Dashboard/Dashboard.js
+++ src/componet/Dashboard/Dashboard.js
@@ -13,13 +13,13 @@ const Dashboard = () => {
     const navigate = useNavigate();
     const { dashboardData, loader } = useSelector((state) => state.dashboard)
     // console.log(dashboardData, "dashboardData")
-    
+
     //===================================useEffect==========================================================
     useEffect(() => {
         dispatch(dashboardList())
     }, [])
-    const navigateHandler = (projectName, versionNumber, projectId,model) => {
-        const redirect=model=="objectdetection"?"/training":model=="Classification"?"/classification-training":"/detection-training"
+    const navigateHandler = (projectName, versionNumber, projectId, model) => {
+        const redirect = model == "objectdetection" ? "/training" : model == "Classification" ? "/classification-training" : "/detection-training"
         navigate(redirect, { state: { name: projectName, version: versionNumber, projectId: projectId } })
     }
     return (
@@ -32,13 +32,13 @@ const Dashboard = () => {
                 </div>
                 <div className="AllProjectBox">
                     <div className="CalendarBox">
-                        <CalendarComponent/>
+                        <CalendarComponent />
                     </div>
                     {!loader ?
                         dashboardData?.result?.length > 0 ? (
                             dashboardData?.result?.map((item, i) => {
                                 return (
-                                    <div className="SmallProjectBox" onClick={() => navigateHandler(item?.name, item?.versionNumber, item?._id,item?.model)}>
+                                    <div className="SmallProjectBox" onClick={() => navigateHandler(item?.name, item?.versionNumber, item?._id, item?.model)}>
                                         <h2>{item?.name}</h2>
                                         <p>Version {item?.versionNumber}</p>
                                         <h3>{item?.model}</h3>
@@ -48,7 +48,7 @@ const Dashboard = () => {
                         ) : (
                             <tr>
                                 <td >
-                                    <p style={{marginLeft:"50px",marginTop:"110px"}}><b>No Latest Project Found</b></p>
+                                    <p style={{ marginLeft: "50px", marginTop: "110px" }}><b>No Latest Project Found</b></p>
                                 </td>
                             </tr>
                         ) : <Loader></Loader>}
--- src/config/config.js
+++ src/config/config.js
@@ -6,15 +6,13 @@ module.exports = {
 Url: 'https://nocode-node.oramasolutions.in/api/v1/',
     // Url: 'http://192.168.2.99:3100/api/v1/',
     
-   
-
     // Dynamic service resolver
     getUrl: function (key) {
         const urls = {
            'defectdetection': 'https://nocode-defect-detection.oramasolutions.in/',
             // 'defectdetection': 'http://192.168.0.246:5000/',
-            'classification': 'https://nocode-classification.oramasolutions.in/',
-            // 'classification': 'http://192.168.1.177:5000/',
+            // 'classification': 'https://nocode-classification.oramasolutions.in/',
+            'classification': 'http://192.168.0.246:5008/',
             'text-extraction': 'https://nocode-text-extraction.oramasolutions.in/',
             'objectdetection': 'https://nocode-object-detection.oramasolutions.in/'
             // 'objectdetection': 'http://192.168.0.246:5009/'

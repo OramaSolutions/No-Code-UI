8f7a0e4bfbf8d73bb473c257154cc940659fcc50
adyeetya <2612adityasingh2000@gmail.com>
2025-11-29 11:40:05 +0530

    classification ip fix

--- src/config/config.js
+++ src/config/config.js
@@ -9,10 +9,10 @@ Url: 'https://nocode-node.oramasolutions.in/api/v1/',
     // Dynamic service resolver
     getUrl: function (key) {
         const urls = {
-           'defectdetection': 'https://nocode-defect-detection.oramasolutions.in/',
-            // 'defectdetection': 'http://192.168.0.246:5000/',
-            // 'classification': 'https://nocode-classification.oramasolutions.in/',
-            'classification': 'http://192.168.0.246:5008/',
+        //    'defectdetection': 'https://nocode-defect-detection.oramasolutions.in/',
+            'defectdetection': 'http://192.168.2.46:5000/',
+            'classification': 'https://nocode-classification.oramasolutions.in/',
+            // 'classification': 'http://192.168.0.246:5008/',
             'text-extraction': 'https://nocode-text-extraction.oramasolutions.in/',
             'objectdetection': 'https://nocode-object-detection.oramasolutions.in/'
             // 'objectdetection': 'http://192.168.0.246:5009/'
